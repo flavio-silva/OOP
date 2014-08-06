ce38350a4da5e134e2a69564a450bc80e8119369
Flavio Silva <flv.alves@gmail.com>
2014-08-06 19:29:24 -0300

    criando o array de objetos de clientes utilizando a partir de um arquivo csv

--- Cliente.php
+++ Cliente.php
@@ -2,74 +2,14 @@
 
 class Cliente 
 {
-    private $nome;
-    private $rg;
-    private $cpf;
-    private $logradouro;
-    private $cep;
-    private $cidade;
-    private $estado;
+    public $nome;
+    public $sexo;
+    public $telefone;
+    public $dataNascimento;
+    public $endereco;
+    public $cep;
+    public $bairro;
+    public $cidade;
+    public $estado;
     
-    public function getNome() {
-        return $this->nome;
-    }
-
-    public function getRg() {
-        return $this->rg;
-    }
-
-    public function getCpf() {
-        return $this->cpf;
-    }
-
-    public function getLogradouro() {
-        return $this->logradouro;
-    }
-
-    public function getCep() {
-        return $this->cep;
-    }
-
-    public function getCidade() {
-        return $this->cidade;
-    }
-
-    public function getEstado() {
-        return $this->estado;
-    }
-
-    public function setNome($nome) {
-        $this->nome = $nome;
-        return $this;
-    }
-
-    public function setRg($rg) {
-        $this->rg = $rg;
-        return $this;
-    }
-
-    public function setCpf($cpf) {
-        $this->cpf = $cpf;
-        return $this;
-    }
-
-    public function setLogradouro($logradouro) {
-        $this->logradouro = $logradouro;
-        return $this;
-    }
-
-    public function setCep($cep) {
-        $this->cep = $cep;
-        return $this;
-    }
-
-    public function setCidade($cidade) {
-        $this->cidade = $cidade;
-        return $this;
-    }
-
-    public function setEstado($estado) {
-        $this->estado = $estado;
-        return $this;
-    }
 }
--- public/index.php
+++ public/index.php
@@ -1,5 +1,25 @@
 <?php
 
 include_once __DIR__ . '/../layout/header.php';
+include_once __DIR__ .'/../Cliente.php';
 
+$clientes = file(__DIR__ . '/../clientes.csv');
+$arrayClientes = array();
+for($i = 0; $i < sizeof($clientes); $i++) {
+    $cliente = explode(';',$clientes[$i]);
+    
+    $objCliente = new Cliente();
+    $objCliente->nome = $cliente[0];
+    $objCliente->sexo = $cliente[1];
+    $objCliente->telefone = $cliente[2];
+    $objCliente->endereco = $cliente[3];
+    $objCliente->cep = $cliente[4];
+    $objCliente->bairro = $cliente[5];
+    $objCliente->cidade = $cliente[6];
+    $objCliente->estado = $cliente[7];
+    $objCliente->dataNascimento = $cliente[8];
+    
+    $arrayClientes[] = $objCliente;
+}
+var_dump($arrayClientes);
 include_once __DIR__ . '/../layout/footer.php';
