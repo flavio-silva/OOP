8650c51368a1226ce26323cb93e1c188287984d5
Flavio Silva <flv.alves@gmail.com>
2014-08-07 19:53:54 -0300

    - criando a views home e cliente para listagem dos clientes e informações   detalhadas do mesmo respectivamente - refatorando a index para não conter código referente à criação do array   de objetos de clientes - Colocando o array de clientes dentro do arquivo cliente.php

--- Cliente.php
+++ Cliente.php
@@ -1,7 +1,8 @@
 <?php
 
-class Cliente 
+class Cliente
 {
+    public $id;
     public $nome;
     public $sexo;
     public $telefone;
@@ -12,4 +13,25 @@ class Cliente
     public $cidade;
     public $estado;
     
+}
+
+$clientes = file(__DIR__ . '/clientes.txt');
+
+$arrayClientes = new ArrayObject();
+for($i = 0; $i < sizeof($clientes); $i++) {
+        $cliente = explode("\t",$clientes[$i]);
+
+        $objCliente = new Cliente();
+    $objCliente->id = $i;
+        $objCliente->nome = $cliente[0];
+        $objCliente->sexo = $cliente[1];
+        $objCliente->telefone = $cliente[2];
+        $objCliente->endereco = $cliente[3];
+        $objCliente->cep = $cliente[4];
+        $objCliente->bairro = $cliente[5];
+        $objCliente->cidade = $cliente[6];
+        $objCliente->estado = $cliente[7];
+        $objCliente->dataNascimento = $cliente[8];
+
+        $arrayClientes->offsetSet($i,$objCliente );
 }
--- public/index.php
+++ public/index.php
@@ -1,25 +1,20 @@
 <?php
 
-include_once __DIR__ . '/../layout/header.php';
-include_once __DIR__ .'/../Cliente.php';
+require_once __DIR__ . '/../layout/header.php';
 
-$clientes = file(__DIR__ . '/../clientes.csv');
-$arrayClientes = array();
-for($i = 0; $i < sizeof($clientes); $i++) {
-    $cliente = explode(';',$clientes[$i]);
-    
-    $objCliente = new Cliente();
-    $objCliente->nome = $cliente[0];
-    $objCliente->sexo = $cliente[1];
-    $objCliente->telefone = $cliente[2];
-    $objCliente->endereco = $cliente[3];
-    $objCliente->cep = $cliente[4];
-    $objCliente->bairro = $cliente[5];
-    $objCliente->cidade = $cliente[6];
-    $objCliente->estado = $cliente[7];
-    $objCliente->dataNascimento = $cliente[8];
-    
-    $arrayClientes[] = $objCliente;
+$route = array();
+preg_match('/[[:alpha:]]+((?=\?))?/', $_SERVER['REQUEST_URI'], $route);
+if(empty($route)) {
+    $route = 'home';
+} else {
+    $route = $route[0];
 }
-var_dump($arrayClientes);
-include_once __DIR__ . '/../layout/footer.php';
+
+if(file_exists(__DIR__ . '/../views/' . $route . '.phtml')) {
+    require_once __DIR__ . '/../views/' . $route . '.phtml';
+} else {
+    http_response_code(404);
+    echo 'Página não encontrada';
+}
+
+require_once __DIR__ . '/../layout/footer.php';
