60ec195501f1a5d28f33116fa4c646ae29799752
Flavio Silva <flv.alves@gmail.com>
2014-08-13 18:41:10 -0300

    criando o método persist responsavel pela agregação dos objetos do tipo pessoa

--- src/OOP/DB/Persistencia.php
+++ src/OOP/DB/Persistencia.php
@@ -2,6 +2,10 @@
 
 namespace OOP\DB;
 
+use OOP\Cliente\Pessoa;
+use OOP\Cliente\Types\Fisica;
+use OOP\Cliente\Types\Juridica;
+
 class Persistencia 
 {
     /**
@@ -10,8 +14,25 @@ class Persistencia
      */
     private $db;
     
+    private $fisica = array();
+    private $juridica = array();
+    
     public function __construct(\PDO $db) 
     {
         $this->db = $db;
+    }
+    
+    public function persist(Pessoa $cliente) 
+    {
+        if($cliente instanceof Fisica) {
+            $this->fisica = $cliente;
+        } elseif($cliente instanceof Juridica) {
+            $this->juridica = $cliente;
+        }
+    }
+    
+    public function flush()
+    {
+        
     }
 }
