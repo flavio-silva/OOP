22371f0e4845822695c05f86d703ef61b1a5a135
Flavio Silva <flv.alves@gmail.com>
2014-08-14 20:50:03 -0300

    corrigindo sexo, data de nascimento e cidade e adicionando estado

--- fisica.php
+++ fisica.php
@@ -5,13 +5,14 @@ array (
     'id' => '1',
     'nome' => 'Jacyra',
     'cpf' => '641.132.661-40',
-    'sexo' => 'Masculino',
+    'sexo' => 'F',
     'telefone' => '9516-3359',
     'endereco' => 'Rua Belmiro Braga',
     'cep' => '10285-446',
     'bairro' => 'Vila Átila de Paiva',
-    'cidade' => 'RO',
-    'dataNascimento' => '20/06/2000
+    'cidade' => 'Catas Altas da Noruega',
+    'estado' => 'RO',
+    'dataNascimento' => '2000-06-20
 ',
   ),
   1 => 
@@ -19,13 +20,14 @@ array (
     'id' => '2',
     'nome' => 'Beatrice',
     'cpf' => '472.944.364-01',
-    'sexo' => 'Masculino',
+    'sexo' => 'F',
     'telefone' => '9818-2511',
     'endereco' => 'Av. Sinfrônio Brochado',
     'cep' => '10431-992',
     'bairro' => 'Araguaia',
-    'cidade' => 'SC',
-    'dataNascimento' => '31/01/1997
+    'cidade' => 'Eugenopolis',
+    'estado' => 'SC',
+    'dataNascimento' => '1997-01-31
 ',
   ),
   2 => 
@@ -33,13 +35,14 @@ array (
     'id' => '3',
     'nome' => 'Dalila',
     'cpf' => '730.723.130-16',
-    'sexo' => 'Masculino',
+    'sexo' => 'F',
     'telefone' => '9680-5822',
     'endereco' => 'Rua Goitacazes',
     'cep' => '10764-126',
     'bairro' => 'Nova Granada',
-    'cidade' => 'AM',
-    'dataNascimento' => '17/08/1992
+    'cidade' => 'Inhauma',
+    'estado' => 'AM',
+    'dataNascimento' => '1992-08-17
 ',
   ),
   3 => 
@@ -47,13 +50,14 @@ array (
     'id' => '4',
     'nome' => 'Marco',
     'cpf' => '300.363.333-36',
-    'sexo' => 'Masculino',
+    'sexo' => 'M',
     'telefone' => '9462-9166',
     'endereco' => 'Av. Olegário Maciel',
     'cep' => '10609-618',
     'bairro' => 'Unidas',
-    'cidade' => 'PR',
-    'dataNascimento' => '21/07/1992
+    'cidade' => 'Candeias',
+    'estado' => 'PR',
+    'dataNascimento' => '1992-07-21
 ',
   ),
   4 => 
@@ -61,13 +65,14 @@ array (
     'id' => '5',
     'nome' => 'Nathanael',
     'cpf' => '942.428.576-08',
-    'sexo' => 'Masculino',
+    'sexo' => 'M',
     'telefone' => '9197-6627',
     'endereco' => 'Av. Uruguai',
     'cep' => '10280-887',
     'bairro' => 'Vila Canto do Sabiá',
-    'cidade' => 'MS',
-    'dataNascimento' => '17/07/1985
+    'cidade' => 'Novorizonte',
+    'estado' => 'MS',
+    'dataNascimento' => '1985-07-17
 ',
   ),
   5 => 
@@ -75,13 +80,14 @@ array (
     'id' => '6',
     'nome' => 'Magda',
     'cpf' => '793.132.606-70',
-    'sexo' => 'Feminino',
+    'sexo' => 'F',
     'telefone' => '9385-5344',
     'endereco' => 'Rua Curitiba',
     'cep' => '10476-875',
     'bairro' => 'Guarani',
-    'cidade' => 'AP',
-    'dataNascimento' => '16/04/1962
+    'cidade' => 'Bom Sucesso',
+    'estado' => 'AP',
+    'dataNascimento' => '1962-04-16
 ',
   ),
   6 => 
@@ -89,13 +95,14 @@ array (
     'id' => '7',
     'nome' => 'Adalberto',
     'cpf' => '843.128.695-45',
-    'sexo' => 'Feminino',
+    'sexo' => 'M',
     'telefone' => '9765-9393',
     'endereco' => 'Rua Mato Grosso',
     'cep' => '10676-179',
     'bairro' => 'Vila Tirol',
-    'cidade' => 'MG',
-    'dataNascimento' => '23/05/1986
+    'cidade' => 'Chale',
+    'estado' => 'MG',
+    'dataNascimento' => '1986-05-23
 ',
   ),
   7 => 
@@ -103,13 +110,14 @@ array (
     'id' => '8',
     'nome' => 'Latifa',
     'cpf' => '237.674.279-07',
-    'sexo' => 'Masculino',
+    'sexo' => 'F',
     'telefone' => '9635-9746',
     'endereco' => 'Rua Conceição do Mato Dentro',
     'cep' => '10804-812',
     'bairro' => 'Vila Clóris',
-    'cidade' => 'RN',
-    'dataNascimento' => '03/12/1967
+    'cidade' => 'Pimenta',
+    'estado' => 'RN',
+    'dataNascimento' => '1967-12-03
 ',
   ),
   8 => 
@@ -117,13 +125,14 @@ array (
     'id' => '9',
     'nome' => 'Odette',
     'cpf' => '365.845.837-24',
-    'sexo' => 'Feminino',
+    'sexo' => 'F',
     'telefone' => '9289-6696',
     'endereco' => 'Av. Waldomiro Lobo',
     'cep' => '10687-144',
     'bairro' => 'São Gonçalo',
-    'cidade' => 'PA',
-    'dataNascimento' => '17/01/1991
+    'cidade' => 'Lagoas',
+    'estado' => 'PA',
+    'dataNascimento' => '1991-01-17
 ',
   ),
   9 => 
@@ -131,13 +140,14 @@ array (
     'id' => '10',
     'nome' => 'Jade',
     'cpf' => '674.453.556-00',
-    'sexo' => 'Feminino',
+    'sexo' => 'F',
     'telefone' => '9428-4800',
     'endereco' => 'Rua Bernardo Guimarães',
     'cep' => '10149-101',
     'bairro' => 'Vila Olhos d\'Água',
-    'cidade' => 'MS',
-    'dataNascimento' => '13/10/1984
+    'cidade' => 'Carmo do Rio Claro',
+    'estado' => 'MS',
+    'dataNascimento' => '1984-10-13
 ',
   ),
 );
