eec2aef6d7defec50248802ef4a0a0d65ed871a5
Flavio Silva <flv.alves@gmail.com>
2014-08-14 20:39:16 -0300

    criando o arquivo que retorna um array dos clientes(pessoa fisica)

---
+++ fisica.php
@@ -0,0 +1,143 @@
+<?php
+array (
+  0 => 
+  array (
+    'id' => '1',
+    'nome' => 'Jacyra',
+    'cpf' => '641.132.661-40',
+    'sexo' => 'Masculino',
+    'telefone' => '9516-3359',
+    'endereco' => 'Rua Belmiro Braga',
+    'cep' => '10285-446',
+    'bairro' => 'Vila Átila de Paiva',
+    'cidade' => 'RO',
+    'dataNascimento' => '20/06/2000
+',
+  ),
+  1 => 
+  array (
+    'id' => '2',
+    'nome' => 'Beatrice',
+    'cpf' => '472.944.364-01',
+    'sexo' => 'Masculino',
+    'telefone' => '9818-2511',
+    'endereco' => 'Av. Sinfrônio Brochado',
+    'cep' => '10431-992',
+    'bairro' => 'Araguaia',
+    'cidade' => 'SC',
+    'dataNascimento' => '31/01/1997
+',
+  ),
+  2 => 
+  array (
+    'id' => '3',
+    'nome' => 'Dalila',
+    'cpf' => '730.723.130-16',
+    'sexo' => 'Masculino',
+    'telefone' => '9680-5822',
+    'endereco' => 'Rua Goitacazes',
+    'cep' => '10764-126',
+    'bairro' => 'Nova Granada',
+    'cidade' => 'AM',
+    'dataNascimento' => '17/08/1992
+',
+  ),
+  3 => 
+  array (
+    'id' => '4',
+    'nome' => 'Marco',
+    'cpf' => '300.363.333-36',
+    'sexo' => 'Masculino',
+    'telefone' => '9462-9166',
+    'endereco' => 'Av. Olegário Maciel',
+    'cep' => '10609-618',
+    'bairro' => 'Unidas',
+    'cidade' => 'PR',
+    'dataNascimento' => '21/07/1992
+',
+  ),
+  4 => 
+  array (
+    'id' => '5',
+    'nome' => 'Nathanael',
+    'cpf' => '942.428.576-08',
+    'sexo' => 'Masculino',
+    'telefone' => '9197-6627',
+    'endereco' => 'Av. Uruguai',
+    'cep' => '10280-887',
+    'bairro' => 'Vila Canto do Sabiá',
+    'cidade' => 'MS',
+    'dataNascimento' => '17/07/1985
+',
+  ),
+  5 => 
+  array (
+    'id' => '6',
+    'nome' => 'Magda',
+    'cpf' => '793.132.606-70',
+    'sexo' => 'Feminino',
+    'telefone' => '9385-5344',
+    'endereco' => 'Rua Curitiba',
+    'cep' => '10476-875',
+    'bairro' => 'Guarani',
+    'cidade' => 'AP',
+    'dataNascimento' => '16/04/1962
+',
+  ),
+  6 => 
+  array (
+    'id' => '7',
+    'nome' => 'Adalberto',
+    'cpf' => '843.128.695-45',
+    'sexo' => 'Feminino',
+    'telefone' => '9765-9393',
+    'endereco' => 'Rua Mato Grosso',
+    'cep' => '10676-179',
+    'bairro' => 'Vila Tirol',
+    'cidade' => 'MG',
+    'dataNascimento' => '23/05/1986
+',
+  ),
+  7 => 
+  array (
+    'id' => '8',
+    'nome' => 'Latifa',
+    'cpf' => '237.674.279-07',
+    'sexo' => 'Masculino',
+    'telefone' => '9635-9746',
+    'endereco' => 'Rua Conceição do Mato Dentro',
+    'cep' => '10804-812',
+    'bairro' => 'Vila Clóris',
+    'cidade' => 'RN',
+    'dataNascimento' => '03/12/1967
+',
+  ),
+  8 => 
+  array (
+    'id' => '9',
+    'nome' => 'Odette',
+    'cpf' => '365.845.837-24',
+    'sexo' => 'Feminino',
+    'telefone' => '9289-6696',
+    'endereco' => 'Av. Waldomiro Lobo',
+    'cep' => '10687-144',
+    'bairro' => 'São Gonçalo',
+    'cidade' => 'PA',
+    'dataNascimento' => '17/01/1991
+',
+  ),
+  9 => 
+  array (
+    'id' => '10',
+    'nome' => 'Jade',
+    'cpf' => '674.453.556-00',
+    'sexo' => 'Feminino',
+    'telefone' => '9428-4800',
+    'endereco' => 'Rua Bernardo Guimarães',
+    'cep' => '10149-101',
+    'bairro' => 'Vila Olhos d\'Água',
+    'cidade' => 'MS',
+    'dataNascimento' => '13/10/1984
+',
+  ),
+);
