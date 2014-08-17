72c0f2e3b65a4227dd1a6662a765f308c0860d01
Flavio Silva <flv.alves@gmail.com>
2014-08-17 14:10:25 -0300

    corrigindo parametro do comando set_include_path

--- fixtures.php
+++ fixtures.php
@@ -2,7 +2,7 @@
 $config = parse_ini_file(__DIR__ . '/db.ini');
 $pdo = new PDO("mysql:host={$config['host']};dbname={$config['dbname']}", $config['username'], $config['passwd']);
 
-set_include_path(get_include_path() . PATH_SEPARATOR . '../src/');
+set_include_path(get_include_path() . PATH_SEPARATOR . 'src/');
 spl_autoload_register();
 
 $structure = new OOP\Db\Fixtures\DataStructure($pdo);
