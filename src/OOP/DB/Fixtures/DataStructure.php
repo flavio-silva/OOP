730cb4aaef7e464b19d0c78730e373c089885f42
Flavio Silva <flv.alves@gmail.com>
2014-08-17 13:59:53 -0300

    criando classe para responsavel pela criação de tabelas

---
+++ src/OOP/DB/Fixtures/DataStructure.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace OOP\Db\Fixtures;
+
+class DataStructure 
+{
+    /**     
+     * @var \PDO 
+     */
+    private $db;
+    
+    public function __construct(\PDO $db)
+    {
+        $this->db = $db;
+    }
+    
+    public function dropTableFisica() 
+    {
+        $this->db->query('drop table if exists fisica');
+    }
+    
+    public function createTableFisica() 
+    {
+        $sql = <<<EOD
+        create table fisica (
+            id int auto_increment primary key,
+            nome varchar(255) not null,
+            cpf char(11) not null,
+            sexo enum('F', 'M') not null,
+            nascimento date not null,
+            telefone varchar(15) not null,
+            endereco varchar(255) not null,
+            cep char(8) not null,
+            bairro varchar(30) not null,
+            cidade varchar(30) not null,
+            estado char(2) not null,
+            estrelas tinyint
+        )
+EOD;
+        $this->db->query($sql);
+    }
+    
+    public function dropTableJuridica() 
+    {
+        $this->db->query('drop table if exists juridica');
+    }
+    
+    public function createTableJuridica() 
+    {
+        $sql = <<<EOD
+        create table juridica (
+            id int auto_increment primary key,
+            nome varchar(255) not null,
+                cnpj char(14) not null,	
+            telefone varchar(15) not null,
+            endereco varchar(255) not null,
+            cep char(8) not null,
+            bairro varchar(30) not null,
+            cidade varchar(30) not null,
+            estado char(2) not null,
+            estrelas tinyint
+        )
+EOD;
+        $this->db->query($sql);
+    }    
+}
