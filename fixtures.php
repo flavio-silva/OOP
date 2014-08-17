2c5ee509d23b819f1481a3f0835f3e5975e51432
Flavio Silva <flv.alves@gmail.com>
2014-08-17 14:06:26 -0300

    refatorando para trablhar com objetos

--- fixtures.php
+++ fixtures.php
@@ -2,38 +2,12 @@
 $config = parse_ini_file(__DIR__ . '/db.ini');
 $pdo = new PDO("mysql:host={$config['host']};dbname={$config['dbname']}", $config['username'], $config['passwd']);
 
-$pdo->query('drop table if exists fisica');
-$sql = <<<EOD
-create table fisica (
-    id int auto_increment primary key,
-    nome varchar(255) not null,
-    cpf char(11) not null,
-    sexo enum('F', 'M') not null,
-    nascimento date not null,
-    telefone varchar(15) not null,
-    endereco varchar(255) not null,
-    cep char(8) not null,
-    bairro varchar(30) not null,
-    cidade varchar(30) not null,
-    estado char(2) not null,
-    estrelas tinyint
-)
-EOD;
-$pdo->query($sql);
+set_include_path(get_include_path() . PATH_SEPARATOR . '../src/');
+spl_autoload_register();
 
-$pdo->query('drop table if exists juridica');
-$sql = <<<EOD
-create table juridica (
-    id int auto_increment primary key,
-    nome varchar(255) not null,
-	cnpj char(14) not null,	
-    telefone varchar(15) not null,
-    endereco varchar(255) not null,
-    cep char(8) not null,
-    bairro varchar(30) not null,
-    cidade varchar(30) not null,
-    estado char(2) not null,
-    estrelas tinyint
-)    
-EOD;
-$pdo->query($sql);
+$structure = new OOP\Db\Fixtures\DataStructure($pdo);
+
+$structure->dropTableFisica();
+$structure->createTableFisica();
+$structure->dropTableJuridica();
+$structure->createTableJuridica();
